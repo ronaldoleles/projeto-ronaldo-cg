b5ad6859448332ca86bd3efa3881ee05e144bcbe
Ronaldo Júnio Barbosa de Leles <ronalldoa@hotmail.com>
2022-04-19 10:11:54 -0300

    Update main

--- AlgAnalitic/src/todosalgLinha/main.java
+++ AlgAnalitic/src/todosalgLinha/main.java
@@ -7,9 +7,6 @@ package todosalgLinha;
  */
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         new Janela();
     }
