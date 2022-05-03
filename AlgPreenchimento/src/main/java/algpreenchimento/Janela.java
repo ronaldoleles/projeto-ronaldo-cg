3b134358eeec143c23bb2aa39df82976d65f13a3
Ronaldo Júnio Barbosa de Leles <ronalldoa@hotmail.com>
2022-05-03 10:41:16 -0300

    Att de algoritmos

--- AlgPreenchimento/src/main/java/algpreenchimento/Janela.java
+++ AlgPreenchimento/src/main/java/algpreenchimento/Janela.java
@@ -15,8 +15,12 @@ public class Janela extends JFrame{
         //this.setTitle("Algoritmo de varredura");
         
         //roda alg Boundary-Fill
-        this.add(new AlgBoundaryFill());
-        this.setTitle("Algoritmo de Boundary-Fill");
+        //this.add(new AlgBoundaryFill());
+        //this.setTitle("Algoritmo de Boundary-Fill");
+        
+        //roda alg Preenchimento de Areas
+        this.add(new AlgAreas());
+        this.setTitle("Algoritmo de Preenchimento de Areas");
         this.setSize(300,300);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
